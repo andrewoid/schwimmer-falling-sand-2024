086e3020331645ca982ba230990f72211a952345
Andrew Schwimmer <andrew.schwimmer@disneystreaming.com>
2024-03-06 19:39:58 -0500

    added checkstyle plugin and falling left and right

--- src/main/java/schwimmer/fallingsand/Sand.java
+++ src/main/java/schwimmer/fallingsand/Sand.java
@@ -1,8 +1,16 @@
 package schwimmer.fallingsand;
 
+import java.util.Random;
+
 public class Sand {
 
-    private int[][] field = new int[3][3];
+    private final int[][] field;
+
+    private final Random random = new Random();
+
+    public Sand(int width, int height) {
+        field = new int[height][width];
+    }
 
     public String toString() {
         StringBuilder builder = new StringBuilder();
@@ -35,9 +43,22 @@ public class Sand {
         // moves all sand down one square
         for (int y = field.length - 2; y >= 0; y--) {
             for (int x = 0; x < field[y].length; x++) {
-                if (field[y][x] == 1 && field[y + 1][x] == 0) {
-                    field[y][x] = 0;
-                    field[y + 1][x] = 1;
+                if (field[y][x] == 1) {
+                    // does the sand fall straight down?
+                    if (field[y + 1][x] == 0) {
+                        field[y][x] = 0;
+                        field[y + 1][x] = 1;
+                    }
+                    // does the sand fall to the right?
+                    else if (field[y + 1][x + 1] == 0) {
+                        field[y][x] = 0;
+                        field[y + 1][x + 1] = 1;
+                    }
+                    // does the sand fall to the left?
+                    else if (field[y + 1][x + -1] == 0) {
+                        field[y][x] = 0;
+                        field[y + 1][x - 1] = 1;
+                    }
                 }
             }
         }
