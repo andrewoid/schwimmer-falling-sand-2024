d6be078a682e4898d35de38029546dd24be087ca
Andrew Schwimmer <andrew.schwimmer@disneystreaming.com>
2024-03-06 20:05:39 -0500

    added mockito

--- src/main/java/schwimmer/fallingsand/Sand.java
+++ src/main/java/schwimmer/fallingsand/Sand.java
@@ -6,10 +6,16 @@ public class Sand {
 
     private final int[][] field;
 
-    private final Random random = new Random();
+    private final Random random;
 
     public Sand(int width, int height) {
         field = new int[height][width];
+        this.random = new Random();
+    }
+
+    public Sand(int width, int height, Random random) {
+        field = new int[height][width];
+        this.random = random;
     }
 
     public String toString() {
@@ -44,20 +50,33 @@ public class Sand {
         for (int y = field.length - 2; y >= 0; y--) {
             for (int x = 0; x < field[y].length; x++) {
                 if (field[y][x] == 1) {
-                    // does the sand fall straight down?
                     if (field[y + 1][x] == 0) {
+                        // does the sand fall straight down?
                         field[y][x] = 0;
                         field[y + 1][x] = 1;
+                        continue;
                     }
-                    // does the sand fall to the right?
-                    else if (field[y + 1][x + 1] == 0) {
-                        field[y][x] = 0;
-                        field[y + 1][x + 1] = 1;
-                    }
-                    // does the sand fall to the left?
-                    else if (field[y + 1][x + -1] == 0) {
-                        field[y][x] = 0;
-                        field[y + 1][x - 1] = 1;
+
+                    if (random.nextBoolean()) {
+                        if (field[y + 1][x + 1] == 0) {
+                            // does the sand fall to the right?
+                            field[y][x] = 0;
+                            field[y + 1][x + 1] = 1;
+                        } else if (field[y + 1][x + -1] == 0) {
+                            // does the sand fall to the left?
+                            field[y][x] = 0;
+                            field[y + 1][x - 1] = 1;
+                        }
+                    } else {
+                        if (field[y + 1][x + -1] == 0) {
+                            // does the sand fall to the left?
+                            field[y][x] = 0;
+                            field[y + 1][x - 1] = 1;
+                        } else if (field[y + 1][x + 1] == 0) {
+                            // does the sand fall to the right?
+                            field[y][x] = 0;
+                            field[y + 1][x + 1] = 1;
+                        }
                     }
                 }
             }
